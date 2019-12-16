80f3353ddf96185ffd261601b61b4f61371dfa91
danghoa0107 <hoadang0107@gmail.com>
2019-12-16 21:36:53 +0700

    Add the loai

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,15 @@ Route::group(['prefix'=>'admin'],function(){
 		
 		Route::get('danhsach','TheLoaiController@getDanhSach');
 
-		Route::get('sua','TheLoaiController@getSua');
+		Route::get('sua/{id}','TheLoaiController@getSua');
+
+		Route::post('sua/{id}','TheLoaiController@postSua');
 
 		Route::get('them','TheLoaiController@getThem');
+
+		Route::post('them','TheLoaiController@postThem');
+
+		Route::get('xoa/{id}','TheLoaiController@getXoa');
 	});
 
 	Route::group(['prefix'=>'loaitin'],function(){
@@ -63,3 +69,4 @@ Route::group(['prefix'=>'admin'],function(){
 		Route::get('them','UserController@getThem');
 	});
 
+});
