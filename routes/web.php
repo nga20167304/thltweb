3f8845a8799be51ed1388b2b5e1fa33f77fac0de
honghanh2608 <nguyenthihonghanh2608@gmail.com>
2019-12-16 23:47:46 +0700

    list and add slides

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@
 */
 
 use App\TheLoai;
-use App\LoaiTin;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -23,30 +22,18 @@ Route::group(['prefix'=>'admin'],function(){
 		
 		Route::get('danhsach','TheLoaiController@getDanhSach');
 
-		Route::get('sua/{id}','TheLoaiController@getSua');
-
-		Route::post('sua/{id}','TheLoaiController@postSua');
+		Route::get('sua','TheLoaiController@getSua');
 
 		Route::get('them','TheLoaiController@getThem');
-
-		Route::post('them','TheLoaiController@postThem');
-
-		Route::get('xoa/{id}','TheLoaiController@getXoa');
 	});
 
 	Route::group(['prefix'=>'loaitin'],function(){
-
-		Route::get('danhsach','LoaiTinController@getDanhSach');
 		
-		Route::get('sua/{id}','LoaiTinController@getSua');
+		Route::get('danhsach','LoaiTinController@getDanhSach');
 
-		Route::post('sua/{id}','LoaiTinController@postSua');
+		Route::get('sua','LoaiTinController@getSua');
 
 		Route::get('them','LoaiTinController@getThem');
-
-		Route::post('them','LoaiTinController@postThem');
-		
-		Route::get('xoa/{id}','LoaiTinController@getXoa');
 	});
 
 	Route::group(['prefix'=>'slide'],function(){
@@ -65,8 +52,6 @@ Route::group(['prefix'=>'admin'],function(){
 		Route::get('sua','TinTucController@getSua');
 
 		Route::get('them','TinTucController@getThem');
-
-		Route::post('them','TinTucController@postThem');
 	});
 
 	Route::group(['prefix'=>'user'],function(){
@@ -78,12 +63,18 @@ Route::group(['prefix'=>'admin'],function(){
 		Route::get('them','UserController@getThem');
 	});
 
-	Route::group(['prefix'=>'ajax'],function(){
-		Route::get('loaitin/{idTheLoai}','AjaxController@getLoaiTin');
+	Route::group(['prefix'=>'slide'],function(){
+		Route::get('danhsach','SlideController@getDanhSach');
+
+		Route::get('sua/{id}','SlideController@getSua');
+		Route::post('sua/{id}','SlideController@postSua');
+
+		Route::get('them','SlideController@getThem');
+		Route::post('them','SlideController@postThem');
+
+		Route::get('xoa/{id}','SlideController@getXoa');
 	});
 });
-
-
 
 // =======
 // Route::get('try',function(){
@@ -104,4 +95,3 @@ Route::group(['prefix'=>'admin'],function(){
 // 	Route::get('xoa/{id}','LoaiTinController@postXoa');
 // });
 // >>>>>>> loai tin
-
