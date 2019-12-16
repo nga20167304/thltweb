9c28d26ea161a03f4303b66c46cb828edf6f290e
nga20167304 <hocmetmuonchet@gmail.com>
2019-12-16 22:32:11 +0700

    repair loaitin

--- resources/views/admin/loaitin/them.blade.php
+++ resources/views/admin/loaitin/them.blade.php
@@ -14,16 +14,17 @@
                     <!-- /.col-lg-12 -->
                     <div class="col-lg-7" style="padding-bottom:120px">
                         @if(count($errors)>0)
-                            <div class="alert alert-danger">
+                            <div class = "alert alert-danger">
                                 @foreach($errors->all() as $err)
-                                    {{$err}}<br>
+                                {{$err}}<br>
+
                                 @endforeach
                             </div>
                         @endif
 
                         @if(session('thongbao'))
                             <div class="alert alert-success">
-                                {{$session('thongbao')}}
+                                {{session('thongbao')}}
                             </div>
                         @endif
                         
