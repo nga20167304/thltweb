c3e7da72e40f1033a4daf5469af3a619fffb429f
honghanh2608 <nguyenthihonghanh2608@gmail.com>
2019-12-19 17:43:55 +0700

    them, sua, xoa slide

--- app/Http/Controllers/SlideController.php
+++ app/Http/Controllers/SlideController.php
@@ -56,4 +56,49 @@ class SlideController extends Controller
         $slide->save();
         return redirect('admin/slide/them')->with('thongbao','Thêm thành công');
     }
+    public function getSua($id){
+        $slide = Slide::find($id);
+        return view('admin.slide.sua',['slide'=>$slide]);
+    }
+
+    public function postSua(Request $request, $id) {
+        $this->validate($request,[
+            'Ten' => 'required',
+            'NoiDung' => 'required',
+        ],[
+            'Ten.required' => 'Bạn chưa nhập tên',
+            'NoiDung.required' => 'Bạn chưa nhập nội dung'
+        ]);
+        $slide = Slide::find($id);
+        $slide->Ten = $request->Ten;
+        $slide->NoiDung = $request->NoiDung;
+        if ($request->has('link')) {
+            $slide->link = $request->link;
+        }
+
+        if ($request->hasFile('Hinh')) {
+            $file = $request->file('Hinh');
+            $duoi = $file->getClientOriginalExtension();
+            if ($duoi != 'jpg' && $duoi != 'png' && $duoi != 'jpeg') {
+                return redirect('admin/slide/them')->with('loi','Bạn chỉ được chọn file có đuôi jpg,png,jpeg');
+            }
+            $name = $file->getClientOriginalName();
+            $Hinh = Str::random(4).'_'. $name;
+            while (file_exists("upload/slide/".$Hinh)) {
+                # code...
+                $Hinh = Str::random(4).'_'. $name;
+            }
+            $file->move("upload/slide",$Hinh);
+            $slide->Hinh = $Hinh;
+        }
+
+        $slide->save();
+        return redirect("admin/slide/sua/$id")->with(['thongbao'=>'Sửa thành công', 'slide' =>$slide]);
+    }
+
+    public function getXoa($id) {
+        $slide = Slide::find($id);
+        $slide->delete();
+        return redirect('admin/slide/danhsach') -> with('thongbao', "Đã xóa ".$slide->Ten);
+    }
 }
